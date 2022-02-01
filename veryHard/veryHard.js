6e89a2d33a4682ab576e7fe08a3bcedadf6d2f31
Ultrafeeder <jakarimstratton@gmail.com>
2022-01-31 23:40:12 -0500

    tried on very hard

--- veryHard/veryHard.js
+++ veryHard/veryHard.js
@@ -1,12 +1,16 @@
-function makeChange(amount) {
-    let numOfMon = 0;
-    let fund = [25, 10, 5, 1];
-    if (amount % fund[0] === 0) {
-        numOfMon = numOfMon + Math.floor(amount / fund[0]);
-    } else if ((amount % fund[0]) % fund[1] === 0) {
-        numOfMon = numOfMon + Math.floor(amount / fund[0]) + Math.floor((amount / fund[0]) / fund[1]);
+function changeMaker(amount) {
+
+    const denom = [100, 50, 25, 10, 5, 1];
+    var result = 0;
+//tried to implement a for...of loop
+    for (var _num of denom) {
+       _num = Math.floor(amount / denom);
+        amount = amount % denom;
+        result = amount + _num;
     }
-    return numOfMon
+    return result;
 }
 
-console.log(makeChange(110));
+console.log(changeMaker(6));
+
+
